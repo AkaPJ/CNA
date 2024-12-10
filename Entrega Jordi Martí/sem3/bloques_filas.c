595d2da8846a3912fe3f6197407a010cb8bf8553
AkaPJ <jordimarticres@gmail.com>
2024-12-10 20:20:28 +0100

    intento pr1

---
+++ Entrega Jordi Martí/sem3/bloques_filas.c
@@ -0,0 +1,131 @@
+
+/**************************************************
+ * Producto matriz-vector distribuido 
+ * con distribución cíclica por bloques de filas.
+ **************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <mpi.h>
+
+#define A(i,j) A_[(i)*n+(j)]
+
+int matrizvector( int m, int n, double *A, double *q, double *z );
+
+double norm2( int n, double *v );
+
+int main( int argc, char *argv[] ) {
+
+  MPI_Init(&argc,&argv);
+  int size, rank; 
+  MPI_Comm_size (MPI_COMM_WORLD, &size); 
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  if( argc<3 ) {
+    if( rank==0 ) printf("Usage: mpirun -np %d %s n block_size\n",size,argv[0]);
+    MPI_Finalize();
+    return 0;
+  }
+  int n;
+  sscanf(argv[1],"%d",&n);
+  int blk;
+  sscanf(argv[2],"%d",&blk);
+  if( blk*size > n ) {
+    if( rank==0 ) printf("blk debería ser más pequeño\n");
+    MPI_Finalize();
+    return 0;
+  }
+
+  double *A_, *y, *y_orig;
+  int blocks_per_process = (n+blk*size-1)/(blk*size); //calcula numero de bloques por proceso
+
+ 
+  double *x = (double *) malloc( n*sizeof(double) );
+  if( rank==0 ) {
+    int m = blocks_per_process*blk*size;
+
+    A_ = (double *) calloc( m*n, sizeof(double) );
+
+    for( int i=0; i<n; i++ ) {
+      A(i,i) = ( (double) rand() ) / RAND_MAX; //generar matriz aleatoria
+      for( int j=i+1; j<n; j++ ) {
+        A(i,j) = A(j,i) = ( (double) rand() ) / RAND_MAX; //igual wur snyrd
+      }
+    }
+    for( int i=0; i<n; i++ ) {
+      x[i] = ( (double) rand() ) / RAND_MAX;
+    }
+    y = (double *) malloc( m*sizeof(double) );
+    /* El vector y_orig es para comprobar el resultado */
+    /* y_orig = A * x */
+    y_orig = (double *) malloc( m*sizeof(double) );
+    matrizvector( m, n, A, x, y_orig );
+  }
+  
+  /* Todos los procesos recibirán los datos en la matriz B */
+  double *B = (double *) malloc( blocks_per_process*blk*n*sizeof(double) );
+
+  /* Creación del tipo de dato MPI */
+  MPI_Datatype blocktype;
+  MPI_Type_vector( blocks_per_process*blk, n, n, MPI_DOUBLE, &blocktype );
+  MPI_Type_commit( &blocktype );
+
+  /* Envío de los datos: A y x */
+  MPI_Scatter( A_, 1, blocktype, B, blocks_per_process*blk*n, MPI_DOUBLE, 0, MPI_COMM_WORLD );
+  MPI_Bcast( x, n, MPI_DOUBLE, 0, MPI_COMM_WORLD );
+
+
+  /* Multiplicación a nivel local del nodo */
+  double *y_local = (double *) malloc( blocks_per_process*blk*sizeof(double) );
+
+
+  /* Hay que crear otro tipo de dato MPI para recoger al vector resultado */
+  MPI_Datatype blocktype_y;
+
+
+  /* Recogida del resultado en el vector y, tal que y = A * x */
+  MPI_Type_vector( blocks_per_process*blk, 1, 1, MPI_DOUBLE, &blocktype_y );
+  MPI_Type_commit( &blocktype_y );
+  MPI_Gather( y_local, blocks_per_process*blk, MPI_DOUBLE, y, 1, blocktype_y, 0, MPI_COMM_WORLD );
+
+
+  //si error 0 entonces ejecucion adecuada
+  if( rank == 0 ) {
+    for( int i=0; i<n; i++ ) {
+      y[i] -= y_orig[i];
+    }
+    printf("Error = %f\n",norm2(n,y));
+  }
+
+
+  free(x);
+  free(B);
+  if( rank==0 ) {
+    free(y_orig);
+    free(y);
+    free(A_);
+  }
+  MPI_Finalize();
+  return 0;
+}
+
+#define B(i,j) B[(i)*n+(j)]
+int matrizvector( int m, int n, double *B, double *q, double *z ) {
+  for( int i=0; i<m; i++ ) {
+    z[i] = 0.0;
+    for( int j=0; j<n; j++ ) {
+      z[i] += B(i,j)*q[j]; 
+    }
+  }
+  return 0;
+}
+
+double norm2( int n, double *v ) {
+  double x = 0.0;
+  for( int i=0; i<n; i++ ) {
+    x += v[i]*v[i];
+  }
+  return sqrt(x);
+}
+
+
--- pr1/practica1.c
+++ pr1/practica1.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <mpi.h>
 //cada posicio consecutiva de memoria pertanyen a la mateixa columna
 
 void generaMatriz(double *m, int n) {
@@ -9,30 +9,88 @@ void generaMatriz(double *m, int n) {
     }
 }
 
-void multseq(double *m, double *n, double *res, int num) {
-    for (int i = 0; i < num; i++) {
-        res[i] = 0;
-        for (int j = 0; j < num; j++) {
-            res[i] += m[j] * n[j];
+void sequential_matrix_multiplication(double* A, double* B, double* C, int n) {
+    for(int i = 0; i < n i++) {
+        for (int j = 0; j < n; j++) {
+            C[i*n+j] = 0.0;
+            for(int k = 0; k < n; k++) {
+                C[i*n+j] += A[i*n+k] * B[k*n+j];
+            }
         }
     }
 }
 
-void muestraMatriz(double *m, int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%f ", m[i]);
+double compute_error(double* sequential_C,double* parallel_C,int n) {
+    double error = 0.0;
+    for(int i = 0; i < n; i++) {
+        error += fabs(sequential_C[i] - parallel_C[i]);
     }
-    printf("\n");
+    return error;
 }
 
 int main(int argc, char *argv[]) {
-    int n = 4;
-    double *m = (double *)malloc(n * sizeof(double));
-    double *num = (double *)malloc(n * sizeof(double));
-    double *res = (double *)malloc(n * sizeof(double));
-    generaMatriz(m, n);
-    generaMatriz(num, n);
-    multseq(m, num, res, n);
-    muestraMatriz(res, n);
-    return 0;
+    int rank,size,n;
+    double *A, *B, *C, *Aloc, *Bloc, *Cloc,*sequential_C;
+    double start, end,time,error;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if(argc != 2) {
+        if(rank == 0) {
+            fprintf(stderr,"Usage: %s <matrix size>\n", argv[0]);
+        }
+        MPI_Finalize();
+        return -1;
+    }
+
+    n = atoi(argv[1]);
+
+    A = (double*)malloc(n*n*sizeof(double));
+    B = (double*)malloc(n*n*sizeof(double));
+    C = (double*)malloc(n*n*sizeof(double));
+    sequential_C = (double*)malloc(n*n*sizeof(double));
+
+    Aloc = (double*)malloc(n*(n/size)*sizeof(double));
+    Bloc = (double*)malloc(n*(n/size)*sizeof(double));
+    Cloc = (double*)malloc(n*(n/size)*sizeof(double));
+
+    if (rank==0) {
+        generaMatriz(A,n);
+        generaMatriz(B,n);
+        sequential_matrix_multiplication(A,B,sequential_C,n);
+    }
+
+    MPI_Bcast(A,n*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
+    MPI_Scatter(B,n*(n/size),MPI_DOUBLE,Bloc,n*(n/size),MPI_DOUBLE,0,MPI_COMM_WORLD);
+
+    start = MPI_Wtime();
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n/size; j++) {
+            Cloc[i*(n/size)+j] = 0.0;
+            for(int k = 0; k < n; k++) {
+                Cloc[i*(n/size)+j] += A[i*n+k] * Bloc[k*(n/size)+j];
+            }
+        }
+    }
+    end = MPI_Wtime();
+    time = end - start;
+
+    MPI_Gather(Cloc,n*(n/size),MPI_DOUBLE,C,n*(n/size),MPI_DOUBLE,0,MPI_COMM_WORLD);
+    if(rank == 0) {
+        printf("Multiplicacion paralela finalizada\n");
+        error = compute_error(sequential_C,C,n);
+        printf("Parallel time: %f\n",time);
+        printf("Error: %f\n",error);
+    }
+    free(A);
+    free(B);
+    free(C);
+    free(Aloc);
+    free(Bloc);
+    free(Cloc);    
+    MPI_Finalize();
+    
 }
