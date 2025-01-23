1191c30f75ad997e26c43ec0437bd2c02fce5749
AkaPJ <jordimarticres@gmail.com>
2025-01-23 11:26:31 +0100

    summa.c definitiu

--- pr3/summa.c
+++ pr3/summa.c
@@ -1,148 +1,259 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <time.h>
-#include <mpi.h>
 
-// Constante de tolerancia para la validación de la multiplicación
-#define TOL 1e-4
-
-// Tamaño global de la matriz cuadrada (definimos que m = n = k)
-int N;
-int myrank;
-
-
-//crea matriz random
-void init_matrix(double *matr, const int rows, const int cols) {
-    srand((unsigned int)time(NULL) + myrank); // Semilla diferente por proceso
-    for (int j = 0; j < rows; ++j) {
-        for (int i = 0; i < cols; ++i) {
-            matr[j*cols + i] = rand() * 1.0 / RAND_MAX;
-        }
+void matrix_multiply(double *A, double *B, double *C, int n) {
+    for (int i = 0; i < n * n; i++) {
+        C[i] = 0.0;
     }
-}
 
-//muestra la matriz por pantalla
-void print_matrix(const int rows, const int cols, const double *matr) {
-    for (int j = 0; j < rows; ++j) {
-        for (int i = 0; i < cols; ++i) {
-            printf("%12.6f", matr[j*cols + i]);
-        }
-        printf("\n");
-    }
-    printf("\n\n\n");
-}
-
-//multiplicacion de matrices
-void matmul_naive(const int m, const int n, const int k, const double *A, const double *B, double *C) {
-    for (int j = 0; j < m; ++j) {
-        for (int i = 0; i < k; ++i) {
-            C[j*k + i] = 0.0;
-            for (int l = 0; l < n; ++l) {
-                C[j*k + i] += A[j*n + l] * B[l*k + i];
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n; k++) {
+            for (int j = 0; j < n; j++) {
+                C[i + j * n] += A[i + k * n] * B[k + j * n];
             }
         }
     }
 }
-//multiplicacion de matrices con summa
-void SUMMA(MPI_Comm comm_cart, const int mb, const int nb, const int kb, double *A_loc, double *B_loc, double *C_loc) {
-    int coords[2];
-    MPI_Cart_coords(comm_cart, myrank, 2, coords);
-    int my_col = coords[0];
-    int my_row = coords[1];
 
-    MPI_Comm row_comm, col_comm;
-    int remain_dims[2] = {1, 0}; // Row communicator
-    MPI_Cart_sub(comm_cart, remain_dims, &row_comm);
-    remain_dims[0] = 0; remain_dims[1] = 1; // Column communicator
-    MPI_Cart_sub(comm_cart, remain_dims, &col_comm);
-
-    double *A_loc_save = (double *)calloc(mb*nb, sizeof(double));
-    double *B_loc_save = (double *)calloc(nb*kb, sizeof(double));
-    double *C_loc_tmp = (double *)calloc(mb*kb, sizeof(double));
-
-    memcpy(A_loc_save, A_loc, mb*nb*sizeof(double));
-    memcpy(B_loc_save, B_loc, nb*kb*sizeof(double));
-    memset(C_loc, 0, mb*kb*sizeof(double));
-
-    int nblks = N / nb;
-    for (int bcast_root = 0; bcast_root < nblks; ++bcast_root) {
-        if (my_col == bcast_root) memcpy(A_loc, A_loc_save, mb*nb*sizeof(double));
-        MPI_Bcast(A_loc, mb*nb, MPI_DOUBLE, bcast_root, row_comm);
-
-        if (my_row == bcast_root) memcpy(B_loc, B_loc_save, nb*kb*sizeof(double));
-        MPI_Bcast(B_loc, nb*kb, MPI_DOUBLE, bcast_root, col_comm);
-
-        matmul_naive(mb, nb, kb, A_loc, B_loc, C_loc_tmp);
-        for (int i = 0; i < mb * kb; ++i) C_loc[i] += C_loc_tmp[i];
+void initialize_matrix(double *matrix, int rows, int cols) {
+    for (int i = 0; i < rows * cols; i++) {
+        matrix[i] = (double)rand() / RAND_MAX * 2.0 - 1.0;
     }
-
-    free(A_loc_save);
-    free(B_loc_save);
-    free(C_loc_tmp);
 }
-//parsea los argumentos
-void parse_cmdline(int argc, char *argv[]) {
-    if (argc != 2) {
-        if (myrank == 0) {
-            fprintf(stderr, "Uso: mpirun -np <num_procesos> ./summa <N>\n");
-            MPI_Abort(MPI_COMM_WORLD, 1);
+
+void print_matrix(const char *name, double *matrix, int rows, int cols) {
+    printf("%s = [\n", name);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%9.4f ", matrix[i * cols + j]);
+        }
+        printf("\n");
+    }
+    printf("]\n");
+}
+
+void verify_results(double *C_parallel, double *C_sequential, int n) {
+    double max_diff = 0.0;
+    for (int i = 0; i < n * n; i++) {
+        double diff = fabs(C_parallel[i] - C_sequential[i]);
+        if (diff > max_diff) max_diff = diff;
+    }
+    printf("Error entre multiplicación paralela y secuencial: %e\n", max_diff);
+}
+
+void block_multiply(double *A_block, double *B_block, double *C_block, int b) {
+    for (int i = 0; i < b; i++) {
+        for (int k = 0; k < b; k++) {
+            for (int j = 0; j < b; j++) {
+                C_block[i + j * b] += A_block[i + k * b] * B_block[k + j * b];
+            }
         }
     }
-    N = atoi(argv[1]);
 }
-//main
-int main(int argc, char *argv[]) {
+
+int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    parse_cmdline(argc, argv);
 
-    int nprocs;
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-    int n_proc_rows = sqrt(nprocs);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // Get process rank
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // Get total number of processes
 
-    if (n_proc_rows * n_proc_rows != nprocs) {
-        fprintf(stderr, "ERROR: El número de procesos debe ser un cuadrado perfecto.\n");
-        MPI_Abort(MPI_COMM_WORLD, 1);
+    if (argc < 3) {
+        if (rank == 0) {
+            printf("Usage: %s <matrix_size> <block_size>\n", argv[0]);
+        }
+        MPI_Finalize();
+        return 1;
     }
 
-    int ndims = 2;
-    int dims[2] = {n_proc_rows, n_proc_rows};
-    int periods[2] = {0, 0};
-    int reorder = 0;
-    MPI_Comm comm_cart;
-    MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, reorder, &comm_cart);
+    int n;
+    int b;
+    sscanf(argv[1], "%d", &n); // Matrix size (n x n)
+    sscanf(argv[2], "%d", &b); // Block size
 
-    int mb = N / n_proc_rows;
-    double *A_loc = (double *)calloc(mb * mb, sizeof(double));
-    double *B_loc = (double *)calloc(mb * mb, sizeof(double));
-    double *C_loc = (double *)calloc(mb * mb, sizeof(double));
-
-    double *C_glob = NULL;
-    if (myrank == 0) C_glob = (double *)calloc(N * N, sizeof(double));
-
-    init_matrix(A_loc, mb, mb);
-    init_matrix(B_loc, mb, mb);
-    print_matrix(N, N, A_loc);
-    print_matrix(N, N, B_loc);
-
-    double tstart = MPI_Wtime();
-    SUMMA(comm_cart, mb, mb, mb, A_loc, B_loc, C_loc);
-    double tend = MPI_Wtime();
-
-    MPI_Gather(C_loc, mb * mb, MPI_DOUBLE, C_glob, mb * mb, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    if (myrank == 0) {
-        printf("La matriz resultado es:\n");
-        print_matrix(N, N, C_glob);
-        printf("Tiempo de ejecución de SUMMA: %f segundos\n", tend - tstart);
-        free(C_glob);
+    if (n % b != 0) {
+        if (rank == 0) {
+            printf("Error: n tiene que ser multiplo de b\n");
+        }
+        MPI_Finalize();
+        return 1;
     }
 
-    free(A_loc);
-    free(B_loc);
-    free(C_loc);
+    int N = n / b;
+
+    if (size != N * N) {
+        if (rank == 0)
+            printf("Error: El número de procesos tiene que ser un cuadrado perfecto\n");
+        MPI_Finalize();
+        return -1;
+    }
+
+    MPI_Comm cart_comm;
+    int dims[2];
+    dims[0] = N;
+    dims[1] = N;
+    int periods[2];
+    periods[0] = 0;
+    periods[1] = 0;
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 1, &cart_comm);
+
+    int coords[2];
+    MPI_Cart_coords(cart_comm, rank, 2, coords);
+
+    double *A = NULL;
+    double *B = NULL;
+    double *C = NULL;
+    double *C_sequential = NULL;
+
+    if (rank == 0) {
+        A = malloc(n * n * sizeof(double));
+        B = malloc(n * n * sizeof(double));
+        C = malloc(n * n * sizeof(double));
+        C_sequential = malloc(n * n * sizeof(double));
+
+        // Inicializar matrices
+        initialize_matrix(A, n, n);
+        initialize_matrix(B, n, n);
+
+        // Imprimir matrices iniciales
+        print_matrix("A", A, n, n);
+        print_matrix("B", B, n, n);
+
+        // Multiplicación secuencial
+        double seq_start_time = MPI_Wtime(); // Tiempo inicial
+        for (int i = 0; i < n * n; i++) {
+            C_sequential[i] = 0.0;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n; k++) {
+                for (int j = 0; j < n; j++) {
+                    C_sequential[i + j * n] += A[i + k * n] * B[k + j * n];
+                }
+            }
+        }
+        double seq_end_time = MPI_Wtime(); // Tiempo final
+        printf("Tiempo secuencial: %f\n", seq_end_time - seq_start_time);
+
+        print_matrix("C_sequential", C_sequential, n, n);
+    }
+
+    // Create row communicator
+    MPI_Comm row_comm;
+    int remain_dims[2] = {0, 1};
+    MPI_Cart_sub(cart_comm, remain_dims, &row_comm);
+
+    // Create column communicator
+    MPI_Comm column_comm;
+    remain_dims[0] = 1;
+    remain_dims[1] = 0;
+    MPI_Cart_sub(cart_comm, remain_dims, &column_comm);
+
+    MPI_Barrier(MPI_COMM_WORLD); // Synchronize processes
+    double start_time = MPI_Wtime();
+
+    // Scatter columns between first row processes
+    double *A_block_column = NULL;
+    double *B_block_column = NULL;
+    if (coords[0] == 0) {
+        A_block_column = malloc(n * b * sizeof(double));
+        B_block_column = malloc(n * b * sizeof(double));
+        MPI_Scatter(A, n * b, MPI_DOUBLE, A_block_column, n * b, MPI_DOUBLE, 0, row_comm);
+        MPI_Scatter(B, n * b, MPI_DOUBLE, B_block_column, n * b, MPI_DOUBLE, 0, row_comm);
+    }
+
+    // Create block type
+    MPI_Datatype block;
+    MPI_Type_vector(b, b, n, MPI_DOUBLE, &block);
+    MPI_Type_commit(&block);
+    MPI_Datatype block_resized;
+    MPI_Type_create_resized(block, 0, b * sizeof(double), &block_resized);
+    MPI_Type_commit(&block_resized);
+
+    // Scatter blocks
+    double *A_block_loc = malloc(b * b * sizeof(double));
+    double *B_block_loc = malloc(b * b * sizeof(double));
+    MPI_Scatter(A_block_column, 1, block_resized, A_block_loc, b * b, MPI_DOUBLE, 0, column_comm);
+    MPI_Scatter(B_block_column, 1, block_resized, B_block_loc, b * b, MPI_DOUBLE, 0, column_comm);
+
+    if (coords[0] == 0) {
+        free(A_block_column);
+        free(B_block_column);
+    }
+
+    double *A_block = malloc(b * b * sizeof(double));
+    double *B_block = malloc(b * b * sizeof(double));
+    double *C_block = malloc(b * b * sizeof(double));
+
+    for (int i = 0; i < b * b; ++i) {
+        C_block[i] = 0.0;
+    }
+
+    for (int i = 0; i < N; ++i) {
+        if (coords[1] == i) {
+            memcpy(A_block, A_block_loc, b * b * sizeof(double));
+        }
+        MPI_Bcast(A_block, b * b, MPI_DOUBLE, i, row_comm);
+
+        if (coords[0] == i) {
+            memcpy(B_block, B_block_loc, b * b * sizeof(double));
+        }
+        MPI_Bcast(B_block, b * b, MPI_DOUBLE, i, column_comm);
+
+        block_multiply(A_block, B_block, C_block, b);
+    }
+
+    MPI_Gather(C_block, b * b, MPI_DOUBLE, C, b * b, MPI_DOUBLE, 0, cart_comm);
+
+    double end_time = MPI_Wtime();
+
+    // In the root process, rearrange blocks to assemble the global matrix
+    if (rank == 0) {
+        printf("Time elapsed: %f seconds\n", end_time - start_time);
+
+        double *temp = (double *)malloc(n * n * sizeof(double));
+        memset(temp, 0, n * n * sizeof(double));
+
+        // Reorder blocks correctly
+        for (int pi = 0; pi < N; pi++) {
+            for (int pj = 0; pj < N; pj++) {
+                // Get the rank of the process in the Cartesian grid
+                int coords[2] = {pj, pi};
+                int block_id;
+                MPI_Cart_rank(cart_comm, coords, &block_id);
+
+                // Process each element of the block
+                for (int i = 0; i < b; i++) {
+                    for (int j = 0; j < b; j++) {
+                        int global_i = pi * b + i;
+                        int global_j = pj * b + j;
+                        int block_idx = i * b + j;
+                        temp[global_i * n + global_j] = C[block_id * b * b + block_idx];
+                    }
+                }
+            }
+        }
+
+        memcpy(C, temp, n * n * sizeof(double));
+        free(temp);
+
+        print_matrix("C_parallel", C, n, n);
+        verify_results(C, C_sequential, n);
+
+        free(C_sequential);
+        free(A);
+        free(B);
+        free(C);
+    }
+
+    free(A_block);
+    free(B_block);
+    free(C_block);
+    free(A_block_loc);
+    free(B_block_loc);
+
     MPI_Finalize();
     return 0;
 }
--- pr3/summaMejorado.c
+++ pr3/summaMejorado.c
@@ -2,152 +2,137 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <time.h>
 #include <mpi.h>
 
-#define TOL 1e-4 // Tolerancia para la validación
-
-int N, myrank;
-
 // Inicializa una matriz con valores aleatorios
-void init_matrix(double *matr, const int rows, const int cols, int seed_offset) {
+void init_matrix(double *matrix, int rows, int cols, int seed_offset) {
     srand((unsigned int)time(NULL) + seed_offset);
-    for (int j = 0; j < rows; ++j) {
-        for (int i = 0; i < cols; ++i) {
-            matr[j * cols + i] = rand() * 1.0 / RAND_MAX;
-        }
+    for (int i = 0; i < rows * cols; i++) {
+        matrix[i] = rand() * 1.0 / RAND_MAX;
     }
 }
 
-// Imprime una matriz
-void print_matrix(const int rows, const int cols, const double *matr) {
-    for (int j = 0; j < rows; ++j) {
-        for (int i = 0; i < cols; ++i) {
-            printf("%12.6f", matr[j * cols + i]);
-        }
-        printf("\n");
-    }
-}
-
-// Multiplicación de matrices básica
-void matmul_naive(const int m, const int n, const int k, const double *A, const double *B, double *C) {
-    for (int j = 0; j < m; ++j) {
-        for (int i = 0; i < k; ++i) {
-            C[j * k + i] = 0.0;
-            for (int l = 0; l < n; ++l) {
-                C[j * k + i] += A[j * n + l] * B[l * k + i];
+// Multiplica dos bloques de matrices y acumula el resultado en C
+void matmul_block(double *A, double *B, double *C, int block_size) {
+    for (int i = 0; i < block_size; i++) {
+        for (int j = 0; j < block_size; j++) {
+            for (int k = 0; k < block_size; k++) {
+                C[i * block_size + j] += A[i * block_size + k] * B[k * block_size + j];
             }
         }
     }
 }
 
-// Implementación del algoritmo SUMMA
-void SUMMA(MPI_Comm comm_cart, const int mb, double *A_loc, double *B_loc, double *C_loc) {
+// Algoritmo SUMMA
+void SUMMA(MPI_Comm comm_cart, int block_size, double *A_block, double *B_block, double *C_block, int n_blocks) {
     int coords[2];
-    MPI_Cart_coords(comm_cart, myrank, 2, coords);
-    int my_col = coords[0];
-    int my_row = coords[1];
+    MPI_Cart_coords(comm_cart, MPI_Comm_rank(comm_cart, MPI_COMM_WORLD), 2, coords);
+    int my_row = coords[0], my_col = coords[1];
 
+    // Crear comunicadores por fila y columna
     MPI_Comm row_comm, col_comm;
-    int remain_dims[2] = {1, 0};
+    int remain_dims[2] = {1, 0}; // Por fila
     MPI_Cart_sub(comm_cart, remain_dims, &row_comm);
-    remain_dims[0] = 0; remain_dims[1] = 1;
+    remain_dims[0] = 0; remain_dims[1] = 1; // Por columna
     MPI_Cart_sub(comm_cart, remain_dims, &col_comm);
 
-    double *A_temp = (double *)malloc(mb * mb * sizeof(double));
-    double *B_temp = (double *)malloc(mb * mb * sizeof(double));
+    // Buffers temporales para las difusiones
+    double *A_temp = malloc(block_size * block_size * sizeof(double));
+    double *B_temp = malloc(block_size * block_size * sizeof(double));
 
-    for (int bcast_root = 0; bcast_root < N / mb; ++bcast_root) {
-        if (my_col == bcast_root) memcpy(A_temp, A_loc, mb * mb * sizeof(double));
-        MPI_Bcast(A_temp, mb * mb, MPI_DOUBLE, bcast_root, row_comm);
+    for (int k = 0; k < n_blocks; k++) {
+        // Difusión en filas
+        if (my_col == k) {
+            memcpy(A_temp, A_block, block_size * block_size * sizeof(double));
+        }
+        MPI_Bcast(A_temp, block_size * block_size, MPI_DOUBLE, k, row_comm);
 
-        if (my_row == bcast_root) memcpy(B_temp, B_loc, mb * mb * sizeof(double));
-        MPI_Bcast(B_temp, mb * mb, MPI_DOUBLE, bcast_root, col_comm);
+        // Difusión en columnas
+        if (my_row == k) {
+            memcpy(B_temp, B_block, block_size * block_size * sizeof(double));
+        }
+        MPI_Bcast(B_temp, block_size * block_size, MPI_DOUBLE, k, col_comm);
 
-        matmul_naive(mb, mb, mb, A_temp, B_temp, C_loc);
+        // Multiplicación local de bloques
+        matmul_block(A_temp, B_temp, C_block, block_size);
     }
 
     free(A_temp);
     free(B_temp);
 }
 
-// Verifica el resultado contra una multiplicación secuencial
-int validate_result(double *C, double *A, double *B, int n) {
-    double *C_seq = (double *)calloc(n * n, sizeof(double));
-    matmul_naive(n, n, n, A, B, C_seq);
-    for (int i = 0; i < n * n; ++i) {
-        if (fabs(C[i] - C_seq[i]) > TOL) {
-            free(C_seq);
-            return 0;
-        }
-    }
-    free(C_seq);
-    return 1;
-}
-
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if (argc != 2) {
-        if (myrank == 0)
+        if (rank == 0) {
             fprintf(stderr, "Uso: mpirun -np <num_procesos> ./summa <N>\n");
+        }
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    N = atoi(argv[1]);
-    int nprocs;
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-    int n_proc_rows = sqrt(nprocs);
-
-    if (n_proc_rows * n_proc_rows != nprocs || N % n_proc_rows != 0) {
-        if (myrank == 0)
-            fprintf(stderr, "ERROR: N debe ser divisible por el número de procesos por fila.\n");
+    int N = atoi(argv[1]); // Tamaño global de la matriz
+    int n_proc = sqrt(size); // Procesos por fila/columna
+    if (n_proc * n_proc != size || N % n_proc != 0) {
+        if (rank == 0) {
+            fprintf(stderr, "ERROR: El número de procesos debe ser un cuadrado perfecto y N debe ser divisible por sqrt(p).\n");
+        }
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    int dims[2] = {n_proc_rows, n_proc_rows};
-    int periods[2] = {0, 0};
+    // Tamaño de los bloques
+    int block_size = N / n_proc;
+    double *A_block = malloc(block_size * block_size * sizeof(double));
+    double *B_block = malloc(block_size * block_size * sizeof(double));
+    double *C_block = calloc(block_size * block_size, sizeof(double));
+
+    double *A_global = NULL, *B_global = NULL, *C_global = NULL;
+    if (rank == 0) {
+        A_global = malloc(N * N * sizeof(double));
+        B_global = malloc(N * N * sizeof(double));
+        C_global = malloc(N * N * sizeof(double));
+        init_matrix(A_global, N, N, 0);
+        init_matrix(B_global, N, N, 1);
+    }
+
+    // Crear tipos derivados para bloques
+    MPI_Datatype block_type, block_type_resized;
+    MPI_Type_vector(block_size, block_size, N, MPI_DOUBLE, &block_type);
+    MPI_Type_create_resized(block_type, 0, block_size * sizeof(double), &block_type_resized);
+    MPI_Type_commit(&block_type_resized);
+
+    // Distribuir bloques de matrices
+    MPI_Scatter(A_global, 1, block_type_resized, A_block, block_size * block_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(B_global, 1, block_type_resized, B_block, block_size * block_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Crear topología cartesiana
+    int dims[2] = {n_proc, n_proc}, periods[2] = {0, 0};
     MPI_Comm comm_cart;
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &comm_cart);
 
-    int mb = N / n_proc_rows;
-    double *A_loc = (double *)malloc(mb * mb * sizeof(double));
-    double *B_loc = (double *)malloc(mb * mb * sizeof(double));
-    double *C_loc = (double *)calloc(mb * mb, sizeof(double));
+    // Ejecutar SUMMA
+    double start = MPI_Wtime();
+    SUMMA(comm_cart, block_size, A_block, B_block, C_block, n_proc);
+    double end = MPI_Wtime();
 
-    double *A_glob = NULL, *B_glob = NULL, *C_glob = NULL;
-    if (myrank == 0) {
-        A_glob = (double *)malloc(N * N * sizeof(double));
-        B_glob = (double *)malloc(N * N * sizeof(double));
-        C_glob = (double *)malloc(N * N * sizeof(double));
-        init_matrix(A_glob, N, N, 0);
-        init_matrix(B_glob, N, N, 1);
+    // Recolectar resultados
+    MPI_Gather(C_block, block_size * block_size, MPI_DOUBLE, C_global, block_size * block_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("Tiempo de ejecución: %f segundos\n", end - start);
+        free(A_global);
+        free(B_global);
+        free(C_global);
     }
 
-    MPI_Scatter(A_glob, mb * mb, MPI_DOUBLE, A_loc, mb * mb, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Scatter(B_glob, mb * mb, MPI_DOUBLE, B_loc, mb * mb, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    double tstart = MPI_Wtime();
-    SUMMA(comm_cart, mb, A_loc, B_loc, C_loc);
-    double tend = MPI_Wtime();
-
-    MPI_Gather(C_loc, mb * mb, MPI_DOUBLE, C_glob, mb * mb, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    if (myrank == 0) {
-        printf("Tiempo de ejecución de SUMMA: %f segundos\n", tend - tstart);
-        if (validate_result(C_glob, A_glob, B_glob, N))
-            printf("Resultado validado correctamente.\n");
-        else
-            printf("Error en la validación del resultado.\n");
-        free(A_glob);
-        free(B_glob);
-        free(C_glob);
-    }
-
-    free(A_loc);
-    free(B_loc);
-    free(C_loc);
+    free(A_block);
+    free(B_block);
+    free(C_block);
+    MPI_Type_free(&block_type_resized);
     MPI_Finalize();
     return 0;
 }
